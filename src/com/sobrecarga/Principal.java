3d51ac2c2832f4386f32e98de0f4f1e5e33abddb
lpatrocinio69 <lpatrocinio@terra.com.br>
2022-01-01 10:37:16 -0300

    Alteração do quadrilatero para retornar um tipo.

--- src/com/sobrecarga/Principal.java
+++ src/com/sobrecarga/Principal.java
@@ -7,11 +7,15 @@ public class Principal {
         double lado1 = 10.0;
         double lado2 = 3.5;
         double altura = 5;
+        double area;
 
         // área do quadrado
-        Quadrilatero.area(lado);
-        Quadrilatero.area(lado1,lado2);
-        Quadrilatero.area(lado1,lado2,altura);
+        area = Quadrilatero.area(lado);
+        System.out.printf("Quadrado de lado = %3.3f - Área =  %4.3f\n",lado,area);
+        area = Quadrilatero.area(lado1,lado2);
+        System.out.printf("Retângulo - base = %3.3f - altura = %3.3f - Área =  %4.3f\n",lado1,lado2,area);
+        area = Quadrilatero.area(lado1,lado2,altura);
+        System.out.printf("Trapézio - base1 = %3.3f - base 2 = %3.3f - altura = %3.3f - Área =  %4.3f\n",lado1,lado2,altura,area);
 
 
 
--- src/com/sobrecarga/Quadrilatero.java
+++ src/com/sobrecarga/Quadrilatero.java
@@ -3,24 +3,27 @@ package com.sobrecarga;
 import java.text.NumberFormat;
 
 public class Quadrilatero {
-    public static void area(double lado){
+    public static double area(double lado){
         double area;
         area = lado*lado;
-        System.out.printf("Quadrado de lado = %3.3f - Área =  %4.3f\n",lado,area);
+        return area;
+
 
     }
 
-    public static void area(double lado1, double lado2){
+    public static double area(double lado1, double lado2){
         double area;
         area = lado1*lado2;
-        System.out.printf("Retângulo - base = %3.3f - altura = %3.3f - Área =  %4.3f\n",lado1,lado2,area);
+        return area;
+
 
     }
 
-    public static void area(double base1, double base2, double altura){
+    public static double area(double base1, double base2, double altura){
         double area;
         area = (base1 + base2)*altura/2.0;
-        System.out.printf("Trapézio - base1 = %3.3f - base 2 = %3.3f - altura = %3.3f - Área =  %4.3f\n",base1,base2,altura,area);
+        return area;
+
 
     }
 
